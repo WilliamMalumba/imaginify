c4f50991e73b014086244ec6603f25238e69312b
Will <2156818@cmontmorency.qc.ca>
2024-08-06 09:55:43 -0400

    Implementing clerk webhooks

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -7,8 +7,8 @@ export interface IUser extends Document {
     email: string;
     username: string;
     photo: string;
-    firstName: string;
-    lastName: string;
+    firstName: string | null;
+    lastName: string | null;
     planId: string;
     creditBalance: number;
     createdAt?: Date;
